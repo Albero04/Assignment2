130a544d74877211d8d00f09522c5d37018adddb
Mark-oth <marco.cossi2004@gmail.com>
2025-05-08 23:59:12 +0200

    Codice e test3
    close #2

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,7 +7,15 @@ package it.unipd.mtss;
 public class IntegerToRoman {
 
     public static String convert(int number){
-      // TODO
-      return null;
+      if (number < 1 || number > 3) {
+        throw new IllegalArgumentException("Il numero deve essere compreso tra 1 e 3");
+    }
+    switch(number)
+    {
+        case 1: return "I";   
+        case 2: return "II";  
+        case 3: return "III"; 
+    }
+    return "";
     }
 }
--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -2,3 +2,37 @@
 // Alberto Pignat 2111044
 // Marco   Cossi  2113204
 ////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+public class IntegerToRomanTest {
+
+  @Test
+  public void testConvert(){
+    //test3
+    //arrange
+    int test1=1;
+    int test2=2;
+    int test3=3;
+    
+    //act
+    String answer1=IntegerToRoman.convert(test1);
+    String answer2=IntegerToRoman.convert(test2);
+    String answer3=IntegerToRoman.convert(test3);
+
+    //assert
+    assertEquals("I", answer1);
+    assertEquals("II", answer2);
+    assertEquals("III", answer3);
+    }
+  
+  @Test
+    public void testIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> IntegerToRoman.convert(0));
+        assertThrows(IllegalArgumentException.class, () -> IntegerToRoman.convert(4));
+        assertThrows(IllegalArgumentException.class, () -> IntegerToRoman.convert(-1));
+    }
+}
+
